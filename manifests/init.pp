e81ed6b958861128e1090a4f0ae96c5bfb1f22d8
Nick Walker <nick.walker@puppetlabs.com>
2016-05-17 06:27:17 -0700

    Cleanup cruft.  Leave only necessary portions for new users

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,6 +1,6 @@
-class rampup_profile_puppetmaster {
-
+class rampup_profile_puppetmaster (
   $hiera_yaml = "${::settings::confdir}/hiera.yaml"
+){
 
   class { 'hiera':
     hierarchy  => [
@@ -15,39 +15,10 @@ class rampup_profile_puppetmaster {
     notify     => Service['pe-puppetserver'],
   }
 
-  ini_setting { 'puppet.conf hiera_config master section' :
-    ensure  => absent,
-    path    => "${::settings::confdir}/puppet.conf",
-    section => 'master',
-    setting => 'hiera_config',
-    value   => $hiera_yaml,
-    notify  => Service['pe-puppetserver'],
-  }
-
   #remove the default hiera.yaml from the code-staging directory
   #after the next code manager deployment it should be removed
   #from the live codedir
   file { '/etc/puppetlabs/code-staging/hiera.yaml' :
     ensure => absent,
   }
-
-  #Lay down update-classes.sh for use in r10k postrun_command
-  #This can be configured via the pe_r10k::postrun key in hiera
-  #This does not apply to code manager
-  file { '/usr/local/bin/update-classes.sh' :
-    ensure => file,
-    source => 'puppet:///modules/rampup_profile_puppetmaster/update-classes.sh',
-    mode   => '0755',
-  }
-
-  #https://docs.puppetlabs.com/puppet/latest/reference/config_file_environment.html#environmenttimeout
-  ini_setting { 'environment_timeout = unlimited':
-    ensure  => present,
-    path    => '/etc/puppetlabs/puppet/puppet.conf',
-    section => 'main',
-    setting => 'environment_timeout',
-    value   => 'unlimited',
-    notify  => Service['pe-puppetserver'],
-  }
-
 }
